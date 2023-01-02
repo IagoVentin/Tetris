0f1a79f6e932991404602fd0c56395b924061867
martinuxx18 <martinquintanarana@gmail.com>
2023-01-02 19:27:47 +0100

    Delete

--- src/iu/VentanaPrincipal.java
+++
@@ -1,7 +0,0 @@
-package iu;
-
-import javax.swing.*;
-
-public class VentanaPrincipal {
-    private JPanel panelXogo;
-}
--- src/modelo/Cadrado.java
+++
@@ -1,9 +0,0 @@
-package modelo;
-
-public class Cadrado {
-
-    public int x;
-
-    public int y;
-
-}
